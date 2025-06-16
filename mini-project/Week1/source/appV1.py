a8ac586fd8a787e790e98628292e4b8ad7450dad
Brendon Maphosa <BrendonM@genstudents.org>
2025-06-16 16:40:05 +0100

    latest week 2 mini project as of EoD 16/06

--- mini-project/Week1/source/appV1.py
+++ mini-project/Week1/source/appV1.py
@@ -1,10 +1,42 @@
 # Print main menu options
 def main_menu_opts():
-    print("welcome to the main menu, what would you like to do: \n 1: View all products, \n 2: Add new products, \n 3: Update existing product, \n 4: Delete a product, \n 0: Exit app")
+    print("welcome to the main menu, what would you like to do: \n 1: Print Products Menu, \n 2: Print Orders Menu, \n 0: Exit app")
+
+# Print main menu options
+def products_menu_opts():
+    print("welcome to the products, what would you like to do: \n 1: View all products, \n 2: Add new products, \n 3: Update existing product, \n 4: Delete a product, \n 0: return to main menu")
+
+# Print main menu options
+def orders_menu_opts():
+    print("welcome to the Orders, what would you like to do: \n 1: Display Orders, \n 2: Create new Order, \n 3: Update Order status, \n 4: Update Existing Order, \n 0: return to main menu")
 
 
 # create the empty product list for viewing purposes
-product_list = []
+product_list = ["Espresso", "Cappuccino", "Latte", "Americano", "Mocha", "Iced Coffee", "Flat White", "Cold Brew", "Croissant",
+ "Blueberry Muffin"]
+
+# create orders menu dictionary data variable
+
+orders = [{'order1': {
+    "customer_name": "Alice Smith",
+    "customer_address": "High Road, MANCHESTER",
+    "customer_phone": "07123456789",
+    "status": "ready for pickup",
+    "item(s)_ordered": ["Latte", "Blueberry Muffin"]
+}},{'order2': {
+    "customer_name": "James Carter",
+    "customer_address": "Oak Street, LIVERPOOL",
+    "customer_phone": "07234567890",
+    "status": "preparing",
+    "item(s)_ordered": ["Flat White", "Croissant"]
+}},{'order3': {
+    "customer_name": "Laura Green",
+    "customer_address": "Maple Avenue, SHEFFIELD",
+    "customer_phone": "07345678901",
+    "status": "delivered",
+    "item(s)_ordered": ["Americano", "Cold Brew", "Blueberry Muffin"]
+    }}]
+
 
 # function for add new products to the products data file
 def add_product():
@@ -49,8 +81,12 @@ def prod_del():
         prod_del()
 
 # function for printing out the up to date list of products
-def print_list():
+def print_prod_list():
     print(product_list)   
+
+# function for printing out the up to date orders
+def print_orders():
+    print(orders)  
 
 # function for input prompt
 def input_prompt():
@@ -71,27 +107,31 @@ def logic_function():
     #return_frm_txt()  #return_frm_txt() # Commented out from V1 as data persistance is not possible from what I know without an external data storage file.
     main_menu_opts()
     first_input = int(input("Input the number corresponding with your desired option > "))
-    while first_input <= 4: # tested in test enviroment without this while loop and code look's like it works as normal so this is actually not necessery.
-        if first_input == 1:
-            print_list()
-            mm_return_func()
-        elif first_input == 2:
+    #while first_input >= 1 : # tested in test enviroment without this while loop and code look's like it works as normal so this is actually not necessery.
+    if first_input == 1:
+        products_menu_opts()
+        prod_menu_input1 = int(input('Select the number associated with your desired option'))
+        if prod_menu_input1 == 1:
+            print_prod_list()
+        elif prod_menu_input1 == 2:
             add_product()
-            #return_frm_txt() # Commented out from V1 as data persistance is not possible from what I know without an external data storage file.
-            mm_return_func()
-        elif first_input == 3:
+        elif prod_menu_input1 == 3:
             prod_update()
-            mm_return_func()
-        elif first_input == 4:
+        elif prod_menu_input1 == 4:
             prod_del()
-            mm_return_func()    
-        elif first_input == 0:
-            print("you have decided to leave the app, goodbye")
-            break
-        else: 
-            print("Thats not a valid option, try again")
-            logic_function()
-        break
+        elif prod_menu_input1 == 0:
+            logic_function
+    elif first_input == 2:
+        orders_menu_opts()
+        order_menu_input1 = int(input('\n Select the number associated with your desired option \n'))
+        if order_menu_input1 == 1:
+            print_orders()
+    elif first_input == 0:
+        print("you have decided to leave the app, goodbye")
+    else: 
+        print("Thats not a valid option, try again")
+        logic_function()
+
 
 # app instantiation func 
 logic_function()
