fd5ecb508b0514d46d1b9d657a4e9b338163a938
Brendon Maphosa <BrendonM@genstudents.org>
2025-07-10 21:40:22 +0100

    began laying the ground work for refactoring the ingestion and persistance functions to work with the csv data files

--- mini-project/week4/source/AppPackage_Week4/data_module.py
+++ mini-project/week4/source/AppPackage_Week4/data_module.py
@@ -1,3 +1,5 @@
+import csv
+
 products = []
 
 orders = []
@@ -10,40 +12,45 @@ couriers = []
 
 
 # ingestion function
-def ingest_function(prodlist, courlist):
+def ingest_function(prodlist, courlist,orderslist):
     temp_prod_list = prodlist
     temp_cour_list = courlist
     with open(
-        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week3/data/products2.txt",
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/products3.csv",
         "r",
     ) as read_object:
         for line in read_object.readlines():
             temp_prod_list.append(line.rstrip("\n"))
         prodlist = temp_prod_list
     with open(
-        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week3/data/couriers2.txt",
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/couriers3.csv",
         "r",
     ) as read_object:
         for line in read_object.readlines():
             temp_cour_list.append(line.rstrip("\n"))
         prodlist = temp_cour_list
+    with open("/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv", "r") as read_object:
+            pass
 
 
 # persistence function
-def persistence_function(prodlist, courlist):
+def persistence_function(prodlist, courlist,orderslist):
     temp_prod_list = prodlist
     temp_cour_list = courlist
+    temp_orders_list = orderslist
     with open(
-        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week3/data/products2.txt",
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/products3.csv",
         "w",
     ) as write_object:
         for line in temp_prod_list[:-1]:
             write_object.write(f"{line}\n")
         write_object.write(temp_prod_list[-1])
     with open(
-        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week3/data/couriers2.txt",
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/couriers3.csv",
         "w",
     ) as write_object:
         for line in temp_cour_list[:-1]:
             write_object.write(f"{line}\n")
         write_object.write(temp_cour_list[-1])
+    with open("/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv", "r") as write_object:
+        pass
