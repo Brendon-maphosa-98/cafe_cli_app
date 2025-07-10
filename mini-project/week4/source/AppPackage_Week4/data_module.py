3bf5fe10f44a5caab34a28da42f2dfb96d35631c
Brendon Maphosa <BrendonM@genstudents.org>
2025-07-10 22:29:17 +0100

    added persistence functionality for products in the persistance function in the week 4 data module

--- mini-project/week4/source/AppPackage_Week4/data_module.py
+++ mini-project/week4/source/AppPackage_Week4/data_module.py
@@ -51,36 +51,40 @@ def ingest_function(prodlist, courlist, orderslist):
 
 
 # persistence function
-def persistence_function(prodlist, courlist, orderslist):
+def persistence_function(prodlist):
     temp_prod_list = prodlist
-    temp_cour_list = courlist
-    temp_orders_list = orderslist
     with open(
         "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/products3.csv",
         "w",
     ) as write_object:
         fieldnames = ["name", "price"]
+        csv.DictWriter(write_object,fieldnames=fieldnames).writeheader()
         for order in temp_prod_list:
-            csv.DictWriter()
-    with open(
-        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/couriers3.csv",
-        "w",
-    ) as write_object:
-        fieldnames = ["name", "phone_number"]
-        for line in temp_cour_list[:-1]:
-            write_object.write(f"{line}\n")
-        write_object.write(temp_cour_list[-1])
-    with open(
-        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv",
-        "r",
-    ) as write_object:
-        fieldnames = [
-            "order_num",
-            "customer_name",
-            "customer_address",
-            "customer_phone",
-            "courier",
-            "status",
-            "items",
-        ]
-        pass
+            csv.DictWriter(write_object,fieldnames=fieldnames).writerow(order)
+    # with open(
+    #     "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/couriers3.csv",
+    #     "w",
+    # ) as write_object:
+    #     fieldnames = ["name", "phone_number"]
+    #     for line in temp_cour_list[:-1]:
+    #         write_object.write(f"{line}\n")
+    #     write_object.write(temp_cour_list[-1])
+    # with open(
+    #     "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv",
+    #     "r",
+    # ) as write_object:
+    #     fieldnames = [
+    #         "order_num",
+    #         "customer_name",
+    #         "customer_address",
+    #         "customer_phone",
+    #         "courier",
+    #         "status",
+    #         "items",
+    #     ]
+    #     pass
+
+
+ingest_function(products,couriers,orders)
+
+persistence_function(products)
