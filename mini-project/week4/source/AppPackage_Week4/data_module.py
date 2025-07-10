0e0226ab31003227c7eb38152d3977ead37361fb
Brendon Maphosa <BrendonM@genstudents.org>
2025-07-10 22:03:26 +0100

    added functionality for ingesting data from orders csv to the ingestion function in the week 4 data module

--- mini-project/week4/source/AppPackage_Week4/data_module.py
+++ mini-project/week4/source/AppPackage_Week4/data_module.py
@@ -12,32 +12,48 @@ couriers = []
 
 
 # ingestion function
-def ingest_function(prodlist,courlist):
+def ingest_function(prodlist, courlist, orderslist):
     temp_prod_list = prodlist
     temp_cour_list = courlist
+    temp_orders_list = orderslist
     with open(
         "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/products3.csv",
         "r",
     ) as read_object:
         for row in csv.DictReader(read_object):
-            temp_prod_list.append({'name': row['name'],'price':row['price']})
+            temp_prod_list.append({"name": row["name"], "price": row["price"]})
     prodlist = temp_prod_list
     with open(
-         "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/couriers3.csv",
-         "r",
-     ) as read_object:
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/couriers3.csv",
+        "r",
+    ) as read_object:
         for row in csv.DictReader(read_object):
-            temp_cour_list.append({'name': row['name'],'phone':row['phone_number']})
+            temp_cour_list.append({"name": row["name"], "phone": row["phone_number"]})
     prodlist = temp_cour_list
-    # with open("/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv", "r") as read_object:
-    #         pass
-ingest_function(products,couriers)
-print(products)
-print('')
-print(couriers)
+    with open(
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv",
+        "r",
+    ) as read_object:
+        for row in csv.DictReader(read_object):
+            temp_orders_list.append(
+                {
+                    f"order{row["order_num"]}": {
+                        "customer_name": row["customer_name"],
+                        "customer_address": row["customer_address"],
+                        "customer_phone": row["customer_phone"],
+                        "courier": row["courier"],
+                        "status": row["status"],
+                        "items": row["items"],
+                    }
+                }
+            )
+    orderslist = temp_orders_list
+
+
+
 
 # persistence function
-def persistence_function(prodlist, courlist,orderslist):
+def persistence_function(prodlist, courlist, orderslist):
     temp_prod_list = prodlist
     temp_cour_list = courlist
     temp_orders_list = orderslist
@@ -55,5 +71,8 @@ def persistence_function(prodlist, courlist,orderslist):
         for line in temp_cour_list[:-1]:
             write_object.write(f"{line}\n")
         write_object.write(temp_cour_list[-1])
-    with open("/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv", "r") as write_object:
+    with open(
+        "/Users/brendon/Documents/Data-Engineering/brendon-portfolio/mini-project/week4/data/orders3.csv",
+        "r",
+    ) as write_object:
         pass
