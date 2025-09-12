b214a9a706ee99a418d5c5d91a8cd4474197e2ff
Brendon <brendonjonasmaphosa@gmail.com>
2025-09-12 19:15:41 +0100

    moved the main menu letterhead from the utils/outputs.main_menu.py directory to the menus/main.py directory. This is in preparation a very small file structure tweek before writing the user input validation code. This is in preparion for the navigation functions being created in each menu file.

--- src/menus/main.py
+++ src/menus/main.py
@@ -1,3 +1,9 @@
-from src.utils.outputs.main_menu import *
+from art import text2art
+
+# variable that will hold the letterhead that will be at the top of the main menu
+
+letterhead = text2art("Welcome To Super Cafe")
+
+# main menu options that the user can select from
 
 main_menu_options = "\n1. Orders Menu\n2. Products Menu\n3. Couriers Menu\n0. Exit\n"
