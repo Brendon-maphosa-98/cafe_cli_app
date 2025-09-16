5ac8655706221c67e8f40aaaa3807ebf429eb331
Brendon <brendonjonasmaphosa@gmail.com>
2025-09-16 20:46:33 +0100

    refactor: print letterhead directly in main function for clarity

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from art import text2art
 # Function to start off the CLI app
 def main():
     # variable that will hold the letterhead that will be at the top of the main menu
-    letterhead = text2art("Welcome To Super Cafe")
+    print(text2art("Welcome To Super Cafe"))
 
 
 if __name__ == "__main__":
