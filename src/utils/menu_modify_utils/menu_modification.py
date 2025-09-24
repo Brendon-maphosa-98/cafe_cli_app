658867943e8434e18e5da282d189e29ca1c648de
Brendon <brendonjonasmaphosa@gmail.com>
2025-09-24 16:12:27 +0100

    refactor: update create_new_item function to accept menu_name parameter and enhance user prompts

--- src/utils/menu_modify_utils/menu_modification.py
+++ src/utils/menu_modify_utils/menu_modification.py
@@ -1,6 +1,8 @@
 # functions to add new items to a list
+import os
 
-def create_new_item():
+
+def create_new_item(menu_name):
     """
     Prompt the user to enter a new item name.
     Returns the entered item name as a string.
@@ -8,11 +10,31 @@ def create_new_item():
     loop = 0
     try:
         while loop == 0:
-            new_item = input("Enter the name of the new item: ").strip().capitalize()  # Remove leading/trailing whitespace and capitalize
+            new_item = (
+                input(f"Enter the name of the new {menu_name[:-1]}, or <Enter> to cancel: ")
+                .strip()
+                .capitalize()
+            )  # Remove leading/trailing whitespace and capitalize
             if not new_item:
-                print("No input provided. Please try again.")
+                print("No input provided. would you like to try again? or return to the previous menu?")
+                choice = input("1. Try again\n2. Return to previous menu\n>>> ")
+                if choice == "1":
+                    os.system("sleep 2")
+                    os.system("clear")
+                    continue
+                elif choice == "2":
+                    loop = 1
+                    return None
+                else:
+                    print("Invalid input, please try again.")
+                    loop = 1
+                    return None
             else:
                 loop = 1
                 return new_item
-    except Exception as e:
-        return f"An error occurred while creating the item: {str(e)}"
+    except KeyboardInterrupt:
+        print("\nInput cancelled by user.")
+        return None
+    except EOFError:
+        print("\nInput terminated unexpectedly.")
+        return None
