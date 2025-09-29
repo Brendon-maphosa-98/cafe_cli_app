beb75e34c0fada37010bd01e77fab715416440f1
Brendon <brendonjonasmaphosa@gmail.com>
2025-09-29 16:49:51 +0100

    refactor: remove unused import of user_prompt_for_new_item in main menu

--- src/menus/main.py
+++ src/menus/main.py
@@ -1,5 +1,4 @@
 from src.utils.menu_utils.menu_choice_selection import menu_selection
-from src.utils.menu_modify_utils.menu_modification import user_prompt_for_new_item
 
 # main menu options that the user can select from
 
