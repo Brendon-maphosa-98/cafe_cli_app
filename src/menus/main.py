8eb0ee8acc7162770c4b3e3a1e8c177edfd5d27c
Brendon <brendonjonasmaphosa@gmail.com>
2025-10-02 16:55:36 +0100

    refactor: replace menu_selection with list_selection_choice in main.py for consistency

--- src/menus/main.py
+++ src/menus/main.py
@@ -1,8 +1,13 @@
-from src.utils.menu_utils.menu_choice_selection import menu_selection
+from src.utils.menu_utils.menu_choice_selection import list_selection_choice
 
 # main menu options that the user can select from
 
+menu_name = "Main"
+
 main_menu_options = ["Orders Menu", "Products Menu", "Couriers Menu", "Exit"]
 
-main_menu_selection = menu_selection(main_menu_options)
+main_menu_selection = list_selection_choice(
+    main_menu_options, f"Select an option from the {menu_name} menu:",
+    allow_zero=True,
+)
 
