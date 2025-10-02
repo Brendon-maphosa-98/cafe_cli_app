c5a4c8f91b98440b2e016137fe71ff3d5bbab565
Brendon <brendonjonasmaphosa@gmail.com>
2025-10-02 16:57:21 +0100

    refactor: remove unused import of numbered_display and related code in products.py

--- src/menus/products.py
+++ src/menus/products.py
@@ -1,7 +1,5 @@
-from src.utils.menu_utils.menu_choice_selection import (
-    list_selection_choice,
-    numbered_display,
-)
+from src.utils.menu_utils.menu_choice_selection import list_selection_choice
+
 
 from src.utils.menu_modify_utils.menu_modification import user_prompt_for_new_item
 
@@ -22,7 +20,5 @@ products_menu_selection = list_selection_choice(
     f"Select an option from the {menu_name} menu:",
     allow_zero=True,
 )
-
-view_products_list = numbered_display(list_of_products, menu_name)
 
 new_product = user_prompt_for_new_item(menu_name)
