99116c0892e22ee130fd3249ca6955aac0eb1ce4
Brendon <brendonjonasmaphosa@gmail.com>
2025-10-03 19:07:24 +0100

    refactor: remove unused delete_item_from_list import and update orders data structure

--- src/menus/orders.py
+++ src/menus/orders.py
@@ -1,5 +1,4 @@
 from src.utils.menu_utils.menu_choice_selection import list_selection_choice
-from src.utils.menu_modify_utils.menu_modification import delete_item_from_list
 
 menu_name = "Orders"
 
@@ -11,7 +10,7 @@ orders_main_menu_options = [
     "Return to Main Menu",
 ]
 
-list_of_orders = []
+dict_of_orders = {}
 
 orders_menu_selection = list_selection_choice(
     orders_main_menu_options,
@@ -19,4 +18,3 @@ orders_menu_selection = list_selection_choice(
     allow_zero=True,
 )
 
-delete_order = delete_item_from_list(menu_name, list_of_orders)
